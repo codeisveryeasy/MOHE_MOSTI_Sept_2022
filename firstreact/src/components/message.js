00ea40b4e9755f31fa8d0fe594192cca4e41a4bc
codeisveryeasy <prafful@airasia.com>
2022-09-12 14:32:10 +0530

    states

--- firstreact/src/components/message.js
+++ firstreact/src/components/message.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MessageSize from './messagesize';
 
 function Message(props) {
+
+    const [likes, updateLikes]  =  useState(0)
+    
+    const increaseLikes=()=>{
+        updateLikes(likes +1)
+    }
+ 
     return ( 
         <div>
             <p>
                 Message {props.children}: {props.msg} 
                 <br />
+                <button onClick={increaseLikes}>Like</button> {likes}
+                <br />
                 <MessageSize msg={props.msg}></MessageSize>
+
             </p>
 
         </div>
