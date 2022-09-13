dba41a909907770cc4c5801405ff097a09d74eb8
codeisveryeasy <prafful@airasia.com>
2022-09-13 11:43:12 +0530

    go examples 04

--- go_examples/03functions.go
+++ go_examples/03functions.go
@@ -7,6 +7,21 @@ func main() {
 	addition(5, 3)
 	result := multiply(4, 10)
 	fmt.Println(result)
+	sum, multiple := maths(4, 4)
+	fmt.Println(sum, " and ", multiple)
+
+	sumNew, multipleNew := mathsNew(8, 5)
+	fmt.Println(sumNew, " and ", multipleNew)
+}
+
+func mathsNew(i1, i2 int) (s int, m int) {
+	s = i1 + i2
+	m = i1 * i2
+	return
+}
+
+func maths(i1 int, i2 int) (int, int) {
+	return i1 + i2, i1 * i2
 }
 
 func multiply(i1, i2 int) int {
