da048abbead09ceedcd233446cd8649e3960e7c9
codeisveryeasy <prafful@airasia.com>
2022-09-13 11:59:16 +0530

    go examples 06

---
+++ go_examples/06arrays.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	//declare array
+	//var arrayName = [sizeOfArray]dataType{elements of array}
+	var scores = [4]int{88, 71, 53, 91}
+	fmt.Println(scores)
+	fmt.Println(len(scores))
+
+	var lifes = [12]int{0: 8, 5: 9, 11: 88}
+	fmt.Println(lifes)
+
+	//use for range to iterate on arrays
+	for index, value := range scores {
+		fmt.Println("Index: ", index, "Value: ", value)
+	}
+
+	//use for range to iterate on arrays
+	for _, value := range scores {
+		fmt.Println("Value: ", value)
+	}
+}
