6c4bc13f905128e7686fb2b64ee933d208285ebb
codeisveryeasy <prafful@airasia.com>
2022-09-14 09:52:06 +0530

    API component 02

--- firstreact/src/components/api.js
+++ firstreact/src/components/api.js
@@ -1,6 +1,25 @@
+import axios from 'axios';
 import React from 'react';
+import { useEffect } from 'react';
+
+
 
 function ConsumeCatalogAPI() {
+
+    useEffect(()=>{
+        //use axios here to call API
+        axios.get("http://localhost:8888/catalog/all")
+        //if response comes back -> .then() will be executed
+            .then((response)=>{
+                console.log(response)
+            })
+        //if error comes back -> .catch() will be executed    
+            .catch((error)=>{
+                console.log(error)
+            })
+    },[])
+
+
     return ( 
         <div>
             <h1>I will consume catalog API</h1>
