eec737e700aaf95f9b9bc73ef034f1e061202103
codeisveryeasy <prafful@airasia.com>
2022-09-14 11:27:40 +0530

    API component 04

--- firstreact/src/components/api.js
+++ firstreact/src/components/api.js
@@ -1,17 +1,27 @@
 import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
 import { useEffect } from 'react';
 
 
 
 function ConsumeCatalogAPI() {
 
+    const [catalogItems, updateCatalogItems] = useState([])
+
+
     useEffect(()=>{
         //use axios here to call API
         axios.get("http://localhost:8888/catalog/all")
         //if response comes back -> .then() will be executed
             .then((response)=>{
-                console.log(response)
+                //console.log(response)
+                console.log(catalogItems)
+                console.log("before state update")
+                console.log(response.data)
+                updateCatalogItems(response.data)
+                console.log("after state update")
+                console.log(catalogItems)
             })
         //if error comes back -> .catch() will be executed    
             .catch((error)=>{
